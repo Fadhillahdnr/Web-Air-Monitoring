eed9b9037a9b604b3bda86fc3b75dac8a7df4090
Fadhillahdnrr <m.fadhillah1218@gmail.com>
2026-06-20 12:06:45 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
+            'password'=> bcrypt('admin123'),
         ]);
     }
 }
